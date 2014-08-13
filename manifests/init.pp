b2ddbc8be7d6e401cc50d073fe4dd402cca78066
Matteo Alessani <alessani@gmail.com>
2014-08-13 12:08:13 +0200

    added mdadm dependency

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,6 +2,7 @@ class mountebs {
 
   mount {'umount /mnt':
     name => "/mnt",
+    blockdevice => "/dev/vxdb",
     ensure => "unmounted"
   }
 
@@ -22,5 +23,37 @@ class mountebs {
     ensure => mounted
   }
 
-  Mount['umount /mnt'] -> File['/mnt/beanstalkd'] -> File['/mnt/apps'] -> Mount['/mnt/beanstalkd'] -> Mount['/mnt/apps']
+  package {'mdadm':
+    ensure => present
+  }
+
+  mdadm { '/dev/md0' :
+    ensure    => 'created',
+    devices   => ['/dev/xvdb', '/dev/xvdc'],
+    level     => 0,
+    force     => true
+  }
+
+  filesystem {'/dev/md0':
+    ensure => present,
+    fs_type => 'ext4',
+  }
+
+  exec {'label /tmp':
+    command => "e2label /dev/md0 instance_store",
+    path => "/sbin"
+  }
+
+  mount {'/tmp':
+    ensure => 'mounted',
+    atboot => true
+    device => 'LABEL=instance_store'
+    fstype => 'ext4'
+    options => 'defaults'
+  }
+
+
+  Mount['umount /mnt'] -> File['/mnt/beanstalkd'] -> File['/mnt/apps'] -> Mount['/mnt/beanstalkd'] -> Mount['/mnt/apps'] -> Package['mdadm'] -> Mdadm['/dev/md0'] -> Filesystem['/dev/md0'] -> Exec['label /tmp'] -> Mount['/tmp']
+
+
 }
